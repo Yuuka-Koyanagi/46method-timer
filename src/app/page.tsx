c2077e4a836bba4fae921b401c0ebd0d3d3cec6d
Yuuka-Koyanagi <ykykyk8239@gmail.com>
2025-04-28 12:41:31 +0900

    ♻️ 設定ボタンをコンポーネントに切り出して、リファクタした

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useSettings } from "@/hooks/useSettings";
 import { SettingsModal } from "@/components/SettingsModal";
 import { TimerDisplay } from "@/components/TimerDisplay";
+import { SettingsButton } from "@/components/SettingsButton";
 
 const Home = () => {
   const { showSettings, openSettings, ...settingsModalProps } = useSettings();
@@ -12,15 +13,7 @@ const Home = () => {
       {showSettings && <SettingsModal {...settingsModalProps} />}
 
       <header className="fixed top-0 right-0 p-6">
-        <button
-          className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 transition-colors"
-          onClick={openSettings}
-        >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-          </svg>
-        </button>
+        <SettingsButton onClick={openSettings} />
       </header>
 
       <TimerDisplay />
