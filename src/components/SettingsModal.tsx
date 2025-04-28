ef0dbd2300a570c19fbc5d96811da5f492db463d
Yuuka-Koyanagi <ykykyk8239@gmail.com>
2025-04-28 15:46:19 +0900

    💄 設定ボタンと設定モーダルの決定ボタンのサイズを小さく変更

--- src/components/SettingsModal.tsx
+++ src/components/SettingsModal.tsx
@@ -90,7 +90,7 @@ export const SettingsModal = ({ settings, closeSettings }: SettingsModalProps) =
             className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 transition-colors"
             onClick={closeSettings}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
             </svg>
           </button>
