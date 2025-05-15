77c0c486ba6746610cd152d553a68eac7c3dbb67
Yuuka-Koyanagi <ykykyk8239@gmail.com>
2025-05-15 23:56:24 +0900

    ✨ ホームコンポーネントに抽出メソッド、注湯回数、抽出量の情報を追加し、レイアウトを調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,29 @@ const Home = () => {
   const { time, handleTogglePause } = useTimer();
 
   return (
-    <main className="min-h-screen flex flex-col justify-center items-center">
-      <div className="text-9xl font-bold" onClick={handleTogglePause}>{time}</div>
-      <div>{time > 0 ? "_".repeat(time) : "Happy brewing! ☕️"}</div>
-    </main>
+    <>
+      <main className="min-h-screen flex flex-col justify-center items-center gap-6">
+        <div className="text-9xl font-bold" onClick={handleTogglePause}>{time}</div>
+        <div>{time > 0 ? "_".repeat(time) : "Happy brewing! ☕️"}</div>
+      </main>
+
+      <dl className="min-h-screen flex flex-col justify-center items-center text-2xl gap-6">
+        <div className="flex gap-6">
+          <dt>抽出メソッド</dt>
+          <dd>4:6メソッド</dd>
+        </div>
+
+        <div className="flex gap-6">
+          <dt>注湯回数</dt>
+          <dd>5回</dd>
+        </div>
+
+        <div className="flex gap-6">
+          <dt>抽出量</dt>
+          <dd>300ml</dd>
+        </div>
+      </dl>
+    </>
   );
 }
 
