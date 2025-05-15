085af40127880f73acaa82b2872ca69e0f37c264
Yuuka-Koyanagi <ykykyk8239@gmail.com>
2025-05-15 16:49:24 +0900

    ✨ ホームコンポーネントに時間に応じたメッセージ表示を追加し、レイアウトを調整

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,9 @@ const Home = () => {
   const { time, handleTogglePause } = useTimer();
 
   return (
-    <main className="flex justify-center items-center h-screen">
+    <main className="min-h-screen flex flex-col justify-center items-center">
       <div className="text-9xl font-bold" onClick={handleTogglePause}>{time}</div>
+      <div>{time > 0 ? "_".repeat(time) : "Happy brewing! ☕️"}</div>
     </main>
   );
 }
