2e6e7c332a0909bc26c151e0321647364a9e1c11
Yuuka-Koyanagi <ykykyk8239@gmail.com>
2025-05-20 23:14:15 +0900

    🚸 抽出量の初期値を100から300に変更

--- src/hooks/useSetting.ts
+++ src/hooks/useSetting.ts
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export const useSetting = () => {
   const [pourCount, setPourCount] = useState(5);
-  const [brewVolume, setBrewVolume] = useState(100);
+  const [brewVolume, setBrewVolume] = useState(300);
 
   const handlePourCountChange = (e: React.ChangeEvent<HTMLInputElement>) => setPourCount(Number(e.target.value));
   const handleBrewVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => setBrewVolume(Number(e.target.value));
